a3a70035ab69ed44a460862f95c9405bc628b1a4
siongui <siongui@gmail.com>
2020-07-24 03:26:25 +0800

    add comment

--- css.go
+++ css.go
@@ -12,6 +12,11 @@ var css = `
   padding: 0;
   background-color: white;
   overflow: hidden;
+  // - it seems that it does not matter relative position is set or not.
+  // seems no need to set position if there is no word preview.
+  // - if absolute is set, the width of suggest menu will be "narrow". just fit
+  // the width of longest words
+  // - set position to absolute if there is word preview next to suggest menu
   position: relative;
   left: 0;
   text-align: left;
